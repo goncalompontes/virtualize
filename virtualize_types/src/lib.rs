5ad9e8a41c33d49d51b3a1f04f5cb0860294daf0
goncalompontes <A51642@alunos.isel.pt>
2025-12-30 07:25:15 +0000

    Initial commit. Syntax parsing done. Base traits defined in virtualize_types.

---
+++ virtualize_types/src/lib.rs
@@ -0,0 +1,21 @@
+#![feature(associated_type_defaults)]
+
+pub struct Noop;
+
+impl Vtable for Noop {
+    type Class = Noop;
+}
+
+impl Class for Noop {
+    type Repr = ();
+    type Vtable = Noop;
+}
+
+pub trait Class {
+    type Repr;
+    type Vtable: Vtable = Noop;
+}
+
+pub trait Vtable {
+    type Class: Class;
+}
